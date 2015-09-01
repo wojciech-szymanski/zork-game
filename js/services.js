d5d3774d9b0f2f7f44c92cd615f4cc3c25d49a87
wojciech-szymanski <woszymanski@gmail.com>
2015-09-01 23:17:28 +0100

    Added basic version of game question structure

--- js/services.js
+++ js/services.js
@@ -1,7 +1,32 @@
 zorkGame.factory('DataSource', function() {
     return {
+        data: {
+            "room_id": 12345,
+            "name": "Castle foreyard",
+            "actions": {
+                "look": {
+                    "left": {
+                        "answer": "It looks like there is an entrance to the stables on the left."
+                    },
+                    "at": {
+                        "myself": {
+                            "answer": "Looking good today!"
+                        }
+                    }
+                }
+            }
+        },
         answer: function(command) {
-            return 'I am sorry, I do not understand!';
+            var actions = this.data.actions,
+                i = 0;
+
+            command = command.split(' ');
+            while (actions.hasOwnProperty(command[i])) {
+                actions = actions[command[i]];
+                i++;
+            }
+
+            return actions.hasOwnProperty('answer') ? actions.answer : 'I am sorry, I do not understand!';
         }
     }
 });
